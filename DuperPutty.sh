7bce8ea8d0ed5b3b339154b4a1f799aa5c949d55
Connor Kennedy <cjtkennedy@gmail.com>
2018-05-01 12:35:51 -0500

    Compartmentalize logic and add dependency validation

--- DuperPutty.sh
+++ DuperPutty.sh
@@ -8,12 +8,11 @@ hosts=""
 dns_suffix=""
 sessionname="$(date +%d%h%H%M%S)"
 _params_invalid=false
+_deps_invalid=false
 
-print_example(){
-	echo "EXAMPLE: ./DuperPutty.sh -s SESSIONNAME -u USERNAME -i PATH_TO_FILE_CONTAINING_HOSTS"
-}
 print_help(){
-	printf "$(print_example)\n\n"
+# Prints the help menu
+	printf "EXAMPLE: ./DuperPutty.sh -s SESSIONNAME -u USERNAME -i PATH_TO_FILE_CONTAINING_HOSTS\n\n"
 	echo   "OPTIONS:"
 	echo   "    u   User name"
 	echo   "    s   Session name"
@@ -26,30 +25,29 @@ print_help(){
 	printf "    CTL+B, ] to enter scroll mode if text has gone past scroll back, hit CTL+C to cancel out\n\n"
 }
 read_hosts(){
-	_filepath="$1"
+# Reads contents of the host roster file
+	filepath="$1"
 	bad_separator_regex='\r?\n|\ +'
 	good_separator=' '
-	cat "$_filepath" | tr "$bad_separator_regex" "$good_separator"
+	cat "$filepath" | tr "$bad_separator_regex" "$good_separator"
 }
 ckdependencies(){
-    # GET OS Version first, just support CentOS and Ubuntu for now
-    # Query package manager for byobu and tmux
-    # Spit out install command if they are missing
-
+# Checks that we have the necessary dependencies available for use
     tmuxrc=`which tmux | wc -l`
     byoburc=`which byobu | wc -l`
 
     if [ $tmuxrc = "0" ]; then
-        echo "tmux does not seem to be installed, please install"
-    elif [ $byoburc = "0" ]; then
-        echo "byobu does not seem to be installed, please install"
-    else
-        startbyobu
+        echo "tmux does not seem to be installed"
+        _deps_invalid=true
+    fi
+    if [ $byoburc = "0" ]; then
+        echo "byobu does not seem to be installed"
+        _deps_invalid=true
     fi
 }
 startbyobu(){
+# Fires up byobu and opens a window for each host
 	byobu new-session -d -s $sessionname
-
 	for i in $hosts; do
 		if [ -n "$dns_suffix" ]; then
 			_hname="$i.$dns_suffix"
@@ -59,60 +57,70 @@ startbyobu(){
 		byobu new-window -t $sessionname "ssh -o StrictHostKeyChecking=no -o TCPKeepAlive=yes -o ServerAliveInterval=50 $username@$_hname"
 		byobu select-layout tiled
 	done
-
 	byobu select-window -t 0
 	byobu send-keys exit ENTER
 	byobu attach -t $sessionname
 }
-
-# Parse args
-while getopts ":hs:u:i:d:" opt; do
-	case ${opt} in
-		h ) # Help
-			print_help
-			exit 0
-		;;
-		i ) # Input file
-			_fpath=$OPTARG
-			if [ -z "$_fpath" ]; then
-				echo "Hosts file path was not specified"
+parse_args(){
+# Parses flagged args using getopts
+	while getopts ":hs:u:i:d:" opt; do
+		case ${opt} in
+			h ) # Help
+				print_help
 				exit 0
-			else
-				hosts=$(read_hosts $_fpath)
-			fi
-		;;
-		d ) # DNS suffix
-			dns_suffix=$OPTARG
-		;;
-		s ) # Session name
-			sessionname=$OPTARG
-		;;
-		u ) # Username
-			username=$OPTARG
-		;;
-		\? ) # Default case
-			print_help
-			exit 0
-		;;
-	esac
-done
+			;;
+			i ) # Input file
+				if [ -z "$OPTARG" ]; then
+					echo "Hosts file path was not specified"
+					exit 0
+				else
+					hosts=$(read_hosts $OPTARG)
+				fi
+			;;
+			d ) # DNS suffix
+				dns_suffix=$OPTARG
+			;;
+			s ) # Session name
+				sessionname=$OPTARG
+			;;
+			u ) # Username
+				username=$OPTARG
+			;;
+			\? ) # Default case
+				print_help
+				exit 0
+			;;
+		esac
+	done
+}
+validate_params(){
+# Validates parameters to prevent trouble later
+	if [ -z "$username" ]; then
+		echo "No username was specified"
+		_params_invalid=true
+	fi
+	if [ -z "$hosts" ]; then
+		echo "No hosts were specified"
+		_params_invalid=true
+	fi
+}
 
-# Validate params
-if [ -z "$username" ]; then
-	echo "No username was specified"
-	_params_invalid=true
-fi
-if [ -z "$hosts" ]; then
-	echo "No hosts were specified"
-	_params_invalid=true
+# Ensure we have the necessary dependencies installed
+ckdependencies
+if [ "$_deps_invalid" = true ]; then
+	echo
+	print_help
+	exit 0
 fi
 
-# Tell user about invalid params
+# Parse argmuents and validate them
+parse_args
+validate_params
 if [ "$_params_invalid" = true ]; then
 	echo
 	print_help
 	exit 0
 fi
 
-# Check our dependencies and startbyobu if all is well
-ckdependencies
+# If we got this far, we're valid and we can start byobu
+startbyobu
